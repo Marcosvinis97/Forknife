72d6d6bfb4b6bf9817c386c64e64e96d028ec763
marcosvds <marquinhosvini.s@gmail.com>
2020-03-31 04:37:42 -0300

    Adicionando Comparador16

--- Projetos/C-UnidadeLogicaAritmetica/src/rtl/Comparador16.vhd
+++ Projetos/C-UnidadeLogicaAritmetica/src/rtl/Comparador16.vhd
@@ -18,7 +18,9 @@ architecture rtl of comparador16 is
   -- e componentes (outros módulos) que serao
   -- utilizados nesse modulo.
 
-begin
-  -- Implementação vem aqui!
 
-end architecture;
+  begin
+    -- Implementação vem aqui!
+    ng <= '1' when (a(15) = '1') else '0';
+    zr <= '1' when (a = "0000000000000000") else '0'; 
+  end architecture;
