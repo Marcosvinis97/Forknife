0764e1aacb01c0e14336d48f2ed253ea6ce6713f
luizfelipelazzaron <luizfl@al.insper.edu.br>
2020-05-22 10:30:30 -0300

    Conceito B encaminhado

--- Projetos/F-Computador/src/rtl/ControlUnit.vhd
+++ Projetos/F-Computador/src/rtl/ControlUnit.vhd
@@ -21,17 +21,18 @@ entity ControlUnit is
                                                                      			-- reg. A e Mem. RAM para ALU
                                                                      			-- A  e Mem. RAM para ALU
 		zx, nx, zy, ny, f, no       : out STD_LOGIC;                     	-- sinais de controle da ALU
-		loadA, loadD, loadM, loadPC : out STD_LOGIC              		-- sinais de load do reg. A,
+		loadA, loadD, loadM, loadPC : out STD_LOGIC;              			-- sinais de load do reg. A,
                                                                      			-- reg. D, Mem. RAM e Program Counter
-    );
-end entity;
+		loadS, muxSD		    	: out STD_LOGIC 						-- Conceito B
+		);
+		end entity;
 
 architecture arch of ControlUnit is
 
 begin
 	muxALUI_A <= not instruction(17);
 
-	muxAM <= '0' when instruction(13) = '0' else '1';
+	muxAM <= '0' when ( instruction(13) = '0' ) else '1';
 
 	zx <= instruction(17) and instruction(12);
 	nx <= instruction(17) and instruction(11);
@@ -48,13 +49,6 @@ begin
 		 '1' when  ( instruction(17)='1' and instruction(2 downto 0) = "011"  ) and (ng = '0' and zr = '0' ) else
 		 '1' when  ( instruction(17)='1' and instruction(2 downto 0) = "111"  ) and (ng = '0' and zr = '0' ) else
 		 '0';
-
-	loadA <= '1' when (instruction(17)='1' and instruction(3)='1' ) else '0';
-
-	loadD <= '1' when (instruction(17)='1' and instruction(4)='1' ) else '0';
-
-	loadM <= '1' when (instruction(17)='1' and instruction(5)='1' ) else '0';
-
 
 	muxALUI_A <= not instruction(17);
 
@@ -79,5 +73,11 @@ begin
 	loadD <= '1' when (instruction(17)='1' and instruction(4)='1' ) else '0';
 
 	loadM <= '1' when (instruction(17)='1' and instruction(5)='1' ) else '0';
+
+	-- Conceito B
+
+	muxSD <=  '0' when ( instruction(14) = '0' ) else '1'; 
+
+	loadS <= '1' when (instruction(17)='1' and instruction(6)='1' ) else '0'; 		-- Análogo ao loadD
 
 end architecture;
