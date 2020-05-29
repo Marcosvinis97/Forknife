73b7251f3ebe8658d0711593710b8ca24001e74e
thalials <thalials@al.insper.edu.br>
2020-05-29 09:07:21 -0300

    conceito A

--- Projetos/F-Computador/src/rtl/ControlUnit.vhd
+++ Projetos/F-Computador/src/rtl/ControlUnit.vhd
@@ -16,9 +16,11 @@ entity ControlUnit is
 		zr,ng                       : in STD_LOGIC;                      	-- valores zr(se zero) e
                                                                      			-- ng (se negativo) da ALU
 		muxALUI_A                   : out STD_LOGIC;                     	-- mux que seleciona entre
-                                                                     			-- instrução  e ALU para reg. A
-		muxAM                       : out STD_LOGIC;                     	-- mux que seleciona entre
-                                                                     			-- reg. A e Mem. RAM para ALU
+        
+        MUX_D                       : out STD_LOGIC;                                                             			-- instrução  e ALU para reg. A
+        
+        muxAM                       : out STD_LOGIC;                     	-- mux que seleciona entre
+                                                                    			-- reg. A e Mem. RAM para ALU
                                                                      			-- A  e Mem. RAM para ALU
 		zx, nx, zy, ny, f, no       : out STD_LOGIC;                     	-- sinais de controle da ALU
 		loadA, loadD, loadM, loadPC : out STD_LOGIC;             		-- sinais de load do reg. A,
@@ -32,18 +34,14 @@ begin
 
 	muxALUI_A <= not instruction(17);
 
-	muxAM <= instruction(17) and instruction(13);
-
- 
+    muxAM <= instruction(17) and instruction(13);
 
     zx <= instruction(17) and instruction(12);
     nx <= instruction(17) and instruction(11);
     zy <= instruction(17) and instruction(10);
     ny <= instruction(17) and instruction(9);
     f  <= instruction(17) and instruction(8);
-    no <= instruction(17) and instruction(7);
-
- 
+    no <= instruction(17) and instruction(7); 
 
     loadPC <='1' when  ( instruction(17)='1' and instruction(2 downto 0) = "001"  ) and (ng = '0' and zr = '0' ) else
          '1' when  ( instruction(17)='1' and instruction(2 downto 0) = "010"  ) and (ng = '0' and zr = '1' ) else
@@ -55,18 +53,20 @@ begin
          '1' when  ( instruction(17)='1' and instruction(2 downto 0) = "111"  ) else
          '0';
 
- 
-
-    loadA <= (not instruction(17) or instruction(3));
-	loadD <= '1' when (instruction(17)='1' and instruction(4)='1' ) else '0';
+    loadA <= (not instruction(17) and not instruction(16)) or (instruction(17) and instruction(3));
+--	loadD <= '1' when (instruction(17)='1' and instruction(4)='1' ) else '0';
+    loadD <= (not instruction(17) and instruction(16)) or (instruction(17) and instruction(4));
 
 	loadM <= '1' when (instruction(17)='1' and instruction(5)='1' ) else '0';
-
 
     -- Conceito B
 
 	muxSD <=  '0' when ( instruction(14) = '0' ) else '1'; 
 
 	loadS <= '1' when (instruction(17)='1' and instruction(6)='1' ) else '0'; 		-- Análogo ao loadD
+
+    -- Conceito A
+
+    MUX_D <= instruction(17) and instruction (16);
 
 end architecture;
